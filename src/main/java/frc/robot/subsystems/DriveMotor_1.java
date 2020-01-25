1040305702a9e7bc5e7679ba0414e90f04000316
stones30 <stones30@muhs.edu>
2020-01-25 15:20:46 -0600

    edited this branch to be controlling shooter with PID

--- src/main/java/frc/robot/subsystems/DriveMotor_1.java
+++ src/main/java/frc/robot/subsystems/DriveMotor_1.java
@@ -8,8 +8,11 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 import frc.robot.util.MotorUtil;
@@ -20,22 +23,36 @@ import frc.robot.util.MotorUtil;
 public class DriveMotor_1 extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-  private final TalonSRX Motor_1 = MotorUtil.createTalon(RobotMap.TALON_CAN_9, false);
+  private TalonSRX Motor_1 = MotorUtil.createTalon(RobotMap.TALON_CAN_30 , false);
+  private VictorSPX Motor_2 = MotorUtil.createVictor(RobotMap.VICTOR_CAN_28, true);
+
+  public DriveMotor_1(){
+    Motor_1.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+    Motor_1.configOpenloopRamp(2);
+    Motor_2.configOpenloopRamp(2);
+    Motor_2.follow(Motor_1);
+
+
+  }
 
   public void forward_100() {
     Motor_1.set(ControlMode.PercentOutput, 1);
+    System.out.println(Motor_1.getSelectedSensorVelocity()*600/8192.0+" rpm");
   }
 
   public void forward_75() {
     Motor_1.set(ControlMode.PercentOutput, 0.75);
+    System.out.println(Motor_1.getSelectedSensorVelocity()*600/8192.0+" rpm");
   }
 
   public void forward_50() {
     Motor_1.set(ControlMode.PercentOutput, 0.5);
+    System.out.println(Motor_1.getSelectedSensorVelocity()*600/8192.0+" rpm");
   }
 
   public void forward_25() {
     Motor_1.set(ControlMode.PercentOutput, 0.25);
+    System.out.println(Motor_1.getSelectedSensorVelocity()*600/8192.0+" rpm");
   }
 
   public void stop()
