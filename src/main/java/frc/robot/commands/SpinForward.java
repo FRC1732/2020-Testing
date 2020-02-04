6a187c964e7d2879a7b2f66f7c1a06398867fc04
Matthew Duffy <duffym22@gmail.com>
2020-02-03 22:40:02 -0600

    - Migrated Marcus' commands from Intake-Drivetrain

---
+++ src/main/java/frc/robot/commands/SpinForward.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import frc.robot.RobotContainer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class SpinForward extends CommandBase {
+  /**
+   * Creates a new SpinForward.
+   */
+  public SpinForward() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(RobotContainer.m_Intake);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    RobotContainer.m_Intake.spinForward();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
