9acbc473d567eeff825c7a9ae6f4b95d50884307
Matthew Duffy <duffym22@gmail.com>
2020-02-11 19:14:12 -0600

    Removed imports

--- src/main/java/frc/robot/commands/Intake/StopIntake.java
+++ src/main/java/frc/robot/commands/Intake/StopIntake.java
@@ -8,7 +8,6 @@
 package frc.robot.commands.Intake;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.RobotContainer;
 import frc.robot.subsystems.Intake;
 
 public class StopIntake extends CommandBase {
